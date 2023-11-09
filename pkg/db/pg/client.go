baa4906ebd4c18bb79d80c20f6652da29611bec3
Nazip <nazipm@mail.ru>
2023-11-09 11:48:17 +0300

    init

---
+++ pkg/db/pg/client.go
@@ -0,0 +1,35 @@
+package pg
+
+import (
+	"context"
+	"github.com/nazip/platform_common/pkg/db"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/pkg/errors"
+)
+
+type pgClient struct {
+	masterDBC db.DB
+}
+
+func (c *pgClient) DB() db.DB {
+	return c.masterDBC
+}
+
+func (c *pgClient) Close() error {
+	if c.masterDBC != nil {
+		c.masterDBC.Close()
+	}
+
+	return nil
+}
+func New(ctx context.Context, dsn string) (db.Client, error) {
+	dbc, err := pgxpool.Connect(ctx, dsn)
+	if err != nil {
+		return nil, errors.Errorf("failed to connect to db: %v", err)
+	}
+
+	return &pgClient{
+		masterDBC: &pg{dbc},
+	}, nil
+}
